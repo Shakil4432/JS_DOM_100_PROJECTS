f357bdd10e104696e025e0fe3ffdaefc66d18436
Shakil4432 <shakil4432@gmail.com>
2024-03-06 18:06:23 +0600

    added toast msg by clicked copy

--- 2_Generator_HEX/app2.js
+++ 2_Generator_HEX/app2.js
@@ -1,5 +1,5 @@
 const clickBtn = document.getElementById("click_btn");
-clickBtn.addEventListener('click', () =>{
+clickBtn.addEventListener('click', () => {
     const body = document.getElementById("body");
     const bodyColor = body.style.backgroundColor = generatorRGB();
     const input = document.getElementById("input");
@@ -10,9 +10,40 @@ clickBtn.addEventListener('click', () =>{
     heading.style.color = generatorRGB();
 })
 
-const generatorRGB = () =>{
+const generatorRGB = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `#${r.toString(16).toUpperCase()}${g.toString(16).toUpperCase()}${b.toString(16).toUpperCase()}`;
+}
+
+const createDomElement = () => {
+
+    const input = document.getElementById("input");
+    const inputValue = input.value;
+    navigator.clipboard.writeText(inputValue);
+
+    generateToastMsg(`${inputValue} copied`)
+
+}
+
+const generateToastMsg = (toastMsg) => {
+    const toastMsgDiv = document.getElementById("toastMsgDiv");
+    toastMsgDiv.innerHTML = "";
+    toastMsgDiv.classList.add("toast-msg-slide-in");
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div>
+          <h1>${toastMsg}</h1>
+        </div>
+    `
+    toastMsgDiv.appendChild(div);
+
+    setTimeout(() => {
+
+        toastMsgDiv.classList.remove("toast-msg-slide-in");
+        toastMsgDiv.classList.add("toast-msg-slide-out");
+        toastMsgDiv.innerHTML = "";
+        
+    }, 3000);
 }
